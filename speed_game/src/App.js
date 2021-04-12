374a4b2dbc9cbc1dc27669697f422ad01db3f6a6
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-04-12 11:01:52 +0300

    click handler for score and changing active circle

--- speed_game/src/App.js
+++ speed_game/src/App.js
@@ -3,31 +3,87 @@ import Main from './components/main/Main'
 import React, { Component } from 'react'
 import Circle from './components/circle/Circle'
 import './components/circle/circle.css'
+
+const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
  class App extends Component {
    state = {
      circles: [
-       {id:1, color:'green'},
-       {id:2, color:'red'},
-       {id:3, color:'yellow'},
-       {id:4, color:'blue'}
-     ]
+       {id:0, color:'green'},
+       {id:1, color:'red'},
+       {id:2, color:'yellow'},
+       {id:3, color:'blue'},
+       {id:4, color:'purple'},
+       {id:5, color:'orange'}
+     ],
+     score:12,
+     active:0,
+     previousClicked:4,
+     active:1
+    
    }
+timer = undefined  
+pace = 1500
+
+
+   clickHandler = (id) =>{
+    this.setState({score:this.state.score+1})
+      // this.changeActive(id)
+   }
+   nextCircle = () => {
+     let nextACtive = undefined;
+     do {
+      nextACtive = getRndInteger(1,6);
+     } while (nextACtive === this.state.current);
+
+     this.setState({
+       current:nextACtive,
+      })
+      this.pace *= 0.95
+      this.timer = setTimeout(this.nextCircle,this.pace)
+      console.log("active circle is ", this.state.current)
+
+   }
+  startHandler = () =>{
+    this.nextCircle()
+  }
+
+  endHandler = () => {
+     clearTimeout(this.timer)
+  }
+
   render() {
-    const circlesList = this.state.circles.map( c=>{
-      return <Circle id = {c.id} color = {c.color} />
+    const circlesList = this.state.circles.map( c => { 
+    return  <Circle 
+              key = {c.color}
+              click ={()=>this.clickHandler(c.id)}
+              id = {c.id} 
+              color = {c.color}
+            />
     })
     return (
       <div>
         <h1>Speedtest</h1>
-        <p>Your score is:</p>
+        <p>Your score is: {this.state.score}</p>
         <div className = 'circles'>
           {circlesList}
         </div>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick = {this.startHandler}>Start</button>
+        <button onClick = {this.endHandler}>Stop</button>
       </div>
     )
   }
 }
 export default App
+
+
+//   changeActive(id) {
+//     let {previousClicked, active} = this.state
+//     if(previousClicked === id) return
+//     let newActive = Math.floor(Math.random() *4)
+//     newActive !== active ?
+//      this.setState({...this.state,active:newActive})
+//      :
+//      this.changeActive(id) 
+// }
 
--- speed_game/src/components/circle/Circle.js
+++ speed_game/src/components/circle/Circle.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './circle.css'
 export default function Circle(props) {
     return (
-        <div className ={`circle ${props.color}`}>
+        <div 
+            onClick = {()=>props.click()} 
+            style ={{backgroundColor:props.color}} 
+            className ={`circle`}
+            >
             <p>{props.id}</p>
         </div>
     )
